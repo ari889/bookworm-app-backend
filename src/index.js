169777e92f34b2cd13098ecf2a3cb883c541d11e
Arijit <arijitbanarjee889@gmail.com>
2025-04-17 12:22:29 +0600

    Added corn jobs every 15 munites

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import job from "./lib/cron.js";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+job.start();
 
 app.use(express.json());
 app.use(cors());
